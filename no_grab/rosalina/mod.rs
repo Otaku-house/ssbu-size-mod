e6b25bf56ba1c129676cc0530b9ee15ad6d1c6d2
Otaku-house <81063345+Otaku-house@users.noreply.github.com>
2025-07-06 01:22:48 +0900

    Add files via upload

---
+++ no_grab/rosalina/mod.rs
@@ -0,0 +1,26 @@
+use smash::hash40;
+use smash::phx::Hash40;
+use smash::lib::lua_const::*;
+use smash::app::*;
+use smash::app::lua_bind::*;
+use smash::lua2cpp::{L2CFighterCommon, L2CAgentBase};
+use smashline::*;
+use smash_script::*;
+use smash::phx::Vector3f;
+
+
+#[fighter_frame( agent = FIGHTER_KIND_ROSETTA )]
+fn rosetta_frame(fighter: &mut L2CFighterCommon) {
+    unsafe {
+        HitModule::set_check_catch(fighter.module_accessor, false, 0);
+    }
+}
+
+pub fn install() {
+    smashline::install_agent_frames!(
+        rosetta_frame
+    );
+    smashline::install_acmd_scripts!(
+        
+    );
+}
