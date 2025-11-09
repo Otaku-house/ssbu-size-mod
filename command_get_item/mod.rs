8afaf3ef74825aec0c62a9991377eee49db0285d
Otaku-house <81063345+Otaku-house@users.noreply.github.com>
2025-11-09 21:21:48 +0900

    Add files via upload

---
+++ command_get_item/mod.rs
@@ -0,0 +1,24 @@
+//#![feature(concat_idents)]
+//#![feature(proc_macro_hygiene)]
+//#![feature(asm)]
+//#[warn(stable_features)]
+
+#![feature(
+    concat_idents, 
+    proc_macro_hygiene, 
+    repr_simd, 
+    simd_ffi
+)]
+#![allow(dead_code, non_snake_case, non_upper_case_globals, unused_assignments, unused_macros, unused_must_use, unused_mut, unused_parens, unused_unsafe, unused_variables)]
+
+
+
+mod mushroom;
+mod shadow;
+
+pub fn install() {
+    mushroom::install();
+    shadow::install();
+    
+  }
+
---
+++ command_get_item/shadow/mod.rs
@@ -0,0 +1,37 @@
+use smash::hash40;
+use smash::phx::Hash40;
+use smash::lib::lua_const::*;
+use smash::app::lua_bind::*;
+use smash::app::utility::get_kind;
+use smash::lua2cpp::{L2CFighterCommon, L2CFighterBase};
+use smashline::*;
+use smash_script::*;
+use smash::app::*;
+
+#[fighter_frame_callback]
+pub fn global_fighter_frame(fighter : &mut L2CFighterCommon) {
+    unsafe {
+        let module_accessor = smash::app::sv_system::battle_object_module_accessor(fighter.lua_state_agent);
+        
+        if ControlModule::check_button_on(module_accessor, *CONTROL_PAD_BUTTON_APPEAL_HI) {
+            if ControlModule::check_button_trigger(module_accessor, *CONTROL_PAD_BUTTON_JUMP) {
+                ItemModule::have_item(module_accessor, smash::app::ItemKind(*ITEM_KIND_SHADOW),0,0,true,true);
+                ItemModule::drop_item(module_accessor,0.0,0.0,0);
+            }
+        }
+        if ControlModule::check_button_on(module_accessor, *CONTROL_PAD_BUTTON_JUMP) {
+            if ControlModule::check_button_trigger(module_accessor, *CONTROL_PAD_BUTTON_APPEAL_HI) {
+                ItemModule::have_item(module_accessor, smash::app::ItemKind(*ITEM_KIND_SHADOW),0,0,true,true);
+                ItemModule::drop_item(module_accessor,0.0,0.0,0);
+            }
+        }
+        
+    }
+}
+
+pub fn install() {
+    smashline::install_agent_frame_callbacks!(
+        global_fighter_frame
+        
+    );
+}
